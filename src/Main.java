831361a60a3e56a375c78f3dcf1126116fc36657
Alexandra <alyp@itu.dk>
2024-11-19 15:15:11 +0100

    Added files to new data folder

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         int size = 5;
-        Program p = new Program(size, 800, 750);
+        Program p = new Program(size, 800, 1200);
         World w = p.getWorld();
 
-         w.setTile(new Location(0, 0), new <MyClass>());
+//         w.setTile(new Location(0, 0), new <MyClass>());
 
-        p.setDisplayInformation(<MyClass>.class, new DisplayInformation(<Color>, "<ImageName>"));
+//        p.setDisplayInformation(<MyClass>.class, new DisplayInformation(<Color>, "<ImageName>"));
 
         p.show();
 
