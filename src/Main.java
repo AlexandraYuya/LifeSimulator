276c1afd02feae710f81d899ea441de80c2e4935
Alexandra <alyp@itu.dk>
2024-11-19 15:48:50 +0100

    Tried to work with files

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,27 @@ import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
 import itumulator.world.Location;
 import itumulator.world.World;
+
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.io.File;
 
 public class Main {
 //hej
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int size = 3;
         Program p = new Program(size, 800, 1200);
         World w = p.getWorld();
 
+        File grassA = new File("t1-1a");
+//        File grassB = new File("t1-1b");
+//        File grassC = new File("t1-1c");
+        Scanner sc = new Scanner(grassA);
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(line);
+        }
 //        w.setTile(new Location(0, 0), new <MyClass>());
 
 //        p.setDisplayInformation(<MyClass>.class, new DisplayInformation(<Color>, "<ImageName>"));
