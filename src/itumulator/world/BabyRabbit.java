095ab69eed6518a84954a0e67876eecc773c3aad
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-11-21 12:52:32 +0100

    Update BabyRabbit.java

--- src/itumulator/world/BabyRabbit.java
+++ src/itumulator/world/BabyRabbit.java
@@ -9,9 +9,18 @@ import java.util.Set;
 
 
 public class BabyRabbit implements Actor{
+    private int life;
+    private int energy;
+    private int stepCount;
+
+    public BabyRabbit() {
+        this.life = 10;
+        this.energy = 100;
+        this.stepCount = 0;
+    }
 
     @Override
     public void act(World world) {
-        
+
     }
 }
