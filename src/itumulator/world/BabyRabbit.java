8fc5587290d465f78a73670f67a8944b3c596797
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-11-21 12:51:46 +0100

    Create BabyRabbit.java

---
+++ src/itumulator/world/BabyRabbit.java
@@ -0,0 +1,17 @@
+package itumulator.world;
+import itumulator.executable.Program;
+import itumulator.simulator.Actor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+
+public class BabyRabbit implements Actor{
+
+    @Override
+    public void act(World world) {
+        
+    }
+}
