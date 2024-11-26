338f61ff6e913fd810e28bf4352c7f8ecc42f1d5
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-11-26 15:22:52 +0100

    BEAR

--- src/itumulator/world/Bear.java
+++ src/itumulator/world/Bear.java
@@ -2,7 +2,11 @@ package itumulator.world;
 
 import itumulator.simulator.Actor;
 
+import java.util.Set;
+
 public class Bear implements Actor {
+    int energy;
+
     @Override
     public void act(World world) {
 
@@ -31,6 +35,38 @@ public class Bear implements Actor {
         Location location = new Location(x, y);
         if (!world.containsNonBlocking(location)) {
             world.setTile(location, this);
+        }
+    }
+
+    /**
+     * This method makes it possible for the wolfs to eat rabbits.
+     * @param world The current world.
+     */
+    private void eat(World world) {
+        Location curLocation = world.getLocation(this);
+        Set<Location> surroundingTiles = world.getSurroundingTiles(curLocation);
+
+        if (!surroundingTiles.isEmpty()) {
+            Location rabbitLocation = surroundingTiles.iterator().next();
+            Location berryLocation = surroundingTiles.iterator().next();
+
+            Object isRabbit = world.getTile(rabbitLocation);
+            Object isBerry = world.getTile(berryLocation);
+
+            if (isRabbit instanceof Rabbit && Math.random() <= 0.7) { //here checked if it is a rabbit and adds 70% chance
+                energy += 10;
+                System.out.println("Ate a poor Rabbit - New energy level:" + energy);
+                world.delete(isRabbit);
+                world.move(this, rabbitLocation);
+            }
+            if (isBerry instanceof Berry && Math.random() <= 0.8) { //here checked if it is a berry and adds 80% chance
+                energy += 10;
+                System.out.println("Ate some berries - New energy level:" + energy);
+                world.delete(isBerry);
+            }
+
+            //NEED TO ADD EATING BERRIES
+
         }
     }
 }
--- src/itumulator/world/Berry.java
+++ src/itumulator/world/Berry.java
@@ -19,4 +19,6 @@ public class Berry {
             world.setTile(location, this);
         }
     }
+
+
 }
