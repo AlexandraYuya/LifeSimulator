500b610a89fa42577c49f1d63f5502b4d4741930
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-11-26 21:51:24 +0100

    AddedBush

---
+++ src/itumulator/world/Bush.java
@@ -0,0 +1,23 @@
+package itumulator.world;
+
+public class Bush extends Berry{
+    public Brush(){
+        super();
+    }
+
+    @Override
+    public void placeInWorld(World world) {
+        int size = world.getSize();
+        Location location = null;
+
+        while (location == null || world.getNonBlocking(location) != null) {
+            int x = (int) (Math.random() * size);
+            int y = (int) (Math.random() * size);
+            location = new Location(x, y);
+        }
+        if (!world.containsNonBlocking(location)) {
+            world.setTile(location, this);
+        }
+    }
+
+}
