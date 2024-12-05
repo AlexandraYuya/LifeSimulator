288e95a5c2269b43b8b9167a554760aad79c00d3
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-12-05 11:35:20 +0100

    JUNIT

--- test/itumulator/world/BerryTest.java
+++ test/itumulator/world/BerryTest.java
@@ -7,15 +7,20 @@ import static org.junit.Assert.*;
 
 public class BerryTest {
     private Berry berry;
+    World world;
 
 
     @Before
     public void setUp() {
+        Program program = new Program(5, 800, 500);
+        world = program.getWorld();
         berry = new Berry();
+        berry.placeInWorld(world);
 
     }
 
     @Test public void testBerry() {
-
+    berry.consumeBerries(world);
+    assertTrue(curren);
     }
 }
--- test/itumulator/world/RabbitTest.java
+++ test/itumulator/world/RabbitTest.java
@@ -12,7 +12,7 @@ public class RabbitTest {
 
     @Before
     public void setUp() throws Exception {
-        Program program = new Program(5, 800, 1200);
+        Program program = new Program(5, 800, 600);
         world = program.getWorld();
 
         rabbit = new AdultRabbit();
@@ -22,8 +22,9 @@ public class RabbitTest {
 
     @Test
     public void die() {
-    rabbit.die(world);
-        assertTrue(true);
+        rabbit.life = 0;
+        rabbit.die(world);
+        assertTrue(!world.contains(rabbit));
         //check if it was deleted properly
     }
 }
