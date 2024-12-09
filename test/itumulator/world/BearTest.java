68428c0c132ad08cc54c53969b4529936b0e5f9c
Fernanda Rabenschlag <fernandarabenschlag@gmail.com>
2024-12-09 12:57:36 +0100

    Update BearTest.java

--- test/itumulator/world/BearTest.java
+++ test/itumulator/world/BearTest.java
@@ -1,5 +1,33 @@
 package itumulator.world;
+import Gruppe01.BabyRabbit;
+import Gruppe01.Bear;
 
-public class BearTest
-{
+import itumulator.executable.Program;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class BearTest {
+    Bear bear;
+    World world;
+
+    @Before
+    public void setUp() throws Exception {
+        Program program = new Program(5, 800, 1200);
+        world = program.getWorld();
+
+        bear = new BabyRabbit();
+        bear.placeInWorld(world);
+        bear.act(world);
+    }
+
+    @Test
+    public void die() {
+        bear.die(world);
+        assertTrue(true);
+        //check if it was deleted properly
+    }
+    
+
+
 }
